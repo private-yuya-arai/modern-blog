fa2073f6104e612fc248530edbb4462d3f1a87b8
YUYA ARAI <y.arai.ten@gmail.com>
2026-02-24 02:52:40 +0900

    Updates

---
+++ assets/chunk-4BX2VUAB-CWKWQlKB.js
@@ -0,0 +1,1 @@
+import{_ as i}from"./index-Dn2kYohW.js";function t(c,e){c.accDescr&&e.setAccDescription?.(c.accDescr),c.accTitle&&e.setAccTitle?.(c.accTitle),c.title&&e.setDiagramTitle?.(c.title)}i(t,"populateCommonDb");export{t as p};
