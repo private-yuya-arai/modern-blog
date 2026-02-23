21c1e90eec3d2d67833cca1b321afd01305aab97
YUYA ARAI <y.arai.ten@gmail.com>
2026-02-23 17:10:29 +0900

    Fix: final header overlap and padding

--- dist/assets/stateDiagram-v2-4FDKWEC3-C1F0xenk.js
+++
@@ -1,1 +0,0 @@
-import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-C_qfgYNQ.js";import{_ as i}from"./index-Co4YXEfw.js";import"./chunk-55IACEB6-o9jmDDLL.js";import"./chunk-QN33PNHL-YxsdFjW8.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
